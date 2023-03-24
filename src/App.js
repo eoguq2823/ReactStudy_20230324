cb3cd5ac308c6f9d02c17ad6a00431b3730727f4
eoguq2823 <eoguq9227@naver.com>
2023-03-24 14:14:37 +0900

    리액트

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import InputSample from "./pages/InputSample/InputSample";
+
+// import React from 'react';  //rsc 만들기
+
+function Test1() {
+  return(<h1>test1</h1>)
+}
+
+function Test2() {
+  return(<h1>test2</h1>)
+}
+
 
 function App() {
   return (
     <>
+    <Routes>
+      <Route path="/t1" Component={Test1}/>
+      <Route path="/t2/test" Component={Test2}/>
+      <Route path="/sample/input/1" Component={InputSample}/>
+    </Routes>
     </>
   );
 }
-
 export default App;
